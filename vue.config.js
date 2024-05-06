cd62b6914f2a19a346844baa7ae1b03aaa7e4569
StylishCS <118624912+StylishCS@users.noreply.github.com>
2024-05-06 23:28:44 +0300

    modify allowed hosts

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+module.exports = {
+  devServer: {
+    allowedHosts: [
+      'localhost',
+      'receptionrobot.net',
+      '192.168.1.1',
+    ],
+  },
+};
